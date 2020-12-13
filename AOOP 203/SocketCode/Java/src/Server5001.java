be0f9329260fb8d9f44e29c451a83a907b9e3d76
SiamSarker <sarkersiam2@gmail.com>
2020-12-14 01:55:44 +0600

    One client code done

--- AOOP 203/SocketCode/Java/src/Server5001.java
+++ AOOP 203/SocketCode/Java/src/Server5001.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,9 +12,15 @@ public class Server5001 {
 
             InputStreamReader isr = new InputStreamReader(socket.getInputStream());
             BufferedReader bufferedReader = new BufferedReader(isr);
+
+            OutputStreamWriter o = new OutputStreamWriter(socket.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(o);
+
             String line = bufferedReader.readLine();
             while (line != null) {
                 System.out.println(line);
+                writer.write(line+"\n");
+                writer.flush();
                 line = bufferedReader.readLine();
             }
             socket.close();
